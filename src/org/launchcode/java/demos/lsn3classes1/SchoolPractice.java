41d248668f666b29c9099ae7dbe5c7ce42d54146
kat-billings <katbillings235@gmail.com>
2021-10-26 20:35:12 -0500

    completed chapter four exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,11 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student Kat = new Student();
+        Kat.setName("Kat Billings");
+        Kat.setStudentId(235);
+        Kat.setNumberOfCredits(1);
+        Kat.setGpa(4.0);
+        System.out.println(Kat.getName());
     }
 }
